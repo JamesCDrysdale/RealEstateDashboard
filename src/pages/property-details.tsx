0b3b3a4dbf76047d84f46c77e6bddfce86bc5e48
James Drysdale <james.drysdale@codeclan.com>
2023-02-17 11:58:49 +0000

    Created a card that displays property image :construction:

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -20,7 +20,29 @@ const PropertyDetails = () => {
     if (isError) return <div>Error</div>
 
     return (
-        <div>Property Details</div>
+        <Box
+            borderRadius="15px"
+            padding="20px"
+            bgcolor="#FCFCFC"
+            width="fit-content"
+        >
+            <Typography fontSize={25} fontWeight={700} color="#11142D">
+                Details
+            </Typography>
+
+            <Box mt="20px" display="flex" flexDirection={{ xs: 'column', lg: 'row' }} gap={4}>
+
+                <Box flex={1} maxWidth={750}>
+                    <img 
+                        src={propertyDetails.photo} 
+                        alt={propertyDetails.title} 
+                        height={500}
+                        style={{ objectFit: "cover", borderRadius: "10px" }}
+                    />
+                </Box>
+
+            </Box>
+        </Box>
     )
 }
 
